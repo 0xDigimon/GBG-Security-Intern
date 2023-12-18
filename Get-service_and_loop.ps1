f14212a19d68cb397eae5380266a451e1873b441
Abdelmawla Elamrosy <52350704+0xDigimon@users.noreply.github.com>
2023-12-18 18:31:33 +0200

    Create Get-service_and_loop.ps1

---
+++ Get-service_and_loop.ps1
@@ -0,0 +1,10 @@
+﻿Get-Command -Noun service
+Get-Help Get-Service
+Get-Service | ?{$_.Status -eq "Stopped"}
+Get-Service | ?{$_.Name.StartsWith("A")}
+Get-Service | ?{$_.StartType -eq "Automatic" }
+Get-Service | ?{$_.Status -eq "Running"} | Export-Csv "C:\Users\administrator\Desktop\Running.csv"
+for ($i = 0;$i -lt 20 ; $i++) {
+    $hash = '#' * $i
+    Write-Host "$($hash)"
+}
